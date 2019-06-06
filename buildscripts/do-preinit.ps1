d84371c8f16815c3e9bc6d3cee107f253d07d75b
gjonespf <gjones.pf@gmail.com>
2019-06-06 12:18:42 +1200

    Test verbosity and tweaks

--- buildscripts/do-preinit.ps1
+++ buildscripts/do-preinit.ps1
@@ -4,6 +4,16 @@
 # Functions
 #===============
 
+function Install-PrePrerequisites() {
+    $gitps = Get-Module PowerGit -ErrorAction SilentlyContinue
+
+    if(!($gitps)) {
+        Install-Module PowerGit -Scope CurrentUser
+    } else {
+        Update-Module PowerGit
+    }
+}
+
 function Get-GitCurrentBranch {
     (git symbolic-ref --short HEAD)
 }
@@ -48,18 +58,9 @@ function Invoke-GitFetchRemoteBranches($CurrentBranch) {
     git checkout $CurrentBranch
 }
 
-function Install-PrePrerequisites() {
-    $gitps = Get-Module PowerGit -ErrorAction SilentlyContinue
-
-    if(!($gitps)) {
-        Install-Module PowerGit -Scope CurrentUser
-    } else {
-        Update-Module PowerGit
-    }
-}
-
 # TODO: These should also be in the build env setup
 function Install-DotnetBuildTools() {
+    Write-Host "Installing dotnet core build tools"
     dotnet tool install Octopus.DotNet.Cli --global
     dotnet tool install Cake.Tool --global --version 0.33.0
     dotnet tool install Gitversion.Tool --global --version 4.0.1-beta1-58
@@ -70,8 +71,11 @@ function Install-DotnetBuildTools() {
         if(Test-Path "~/.dotnet/tools") {
             $toolsPath = (Resolve-Path "~/.dotnet/tools").Path
             $env:PATH = $env:PATH + ":$toolsPath"
+        } else {
+            Write-Warning "Couldn't find dotnet core tools directory"
         }
     }
+    Write-Host "Using PATH: $($env:PATH)"
 
     # Minimum cake versions, should be handled by cake install above...
     $cakeVersion = [Version](dotnet-cake --version)
@@ -84,17 +88,34 @@ function Install-DotnetBuildTools() {
 
 # TODO: These should also be in the build env setup
 function Install-DotnetBuildToolsOptional() {
-    # Other possibly useful tools
-    dotnet tool install -g coverlet.console
-    dotnet tool install -g FluentMigrator.DotNet.Cli
-    dotnet tool install -g GitReleaseManager.Tool
-    dotnet tool install -g dotnet-outdated
-    dotnet tool install -g dotnet-t4
-    dotnet tool install -g github-issues-cli
-    dotnet tool install -g NuGetUtils.Tool.Exec
-    dotnet tool install -g dotnet-reportgenerator-globaltool
-}
+    $toolsList = (dotnet tool list -g)
 
+    # Other possibly useful tools
+    if(!($toolsList | ?{ $_ -match "coverlet.console" })) {
+        dotnet tool install -g coverlet.console
+    }
+    if(!($toolsList | ?{ $_ -match "FluentMigrator.DotNet.Cli" })) {
+        dotnet tool install -g FluentMigrator.DotNet.Cli
+    }
+    if(!($toolsList | ?{ $_ -match "GitReleaseManager.Tool" })) {
+        dotnet tool install -g GitReleaseManager.Tool
+    }
+    if(!($toolsList | ?{ $_ -match "dotnet-outdated" })) {
+        dotnet tool install -g dotnet-outdated
+    }
+    if(!($toolsList | ?{ $_ -match "dotnet-t4" })) {
+        dotnet tool install -g dotnet-t4
+    }
+    if(!($toolsList | ?{ $_ -match "github-issues-cli" })) {
+        dotnet tool install -g github-issues-cli
+    }
+    if(!($toolsList | ?{ $_ -match "NuGetUtils.Tool.Exec" })) {
+        dotnet tool install -g NuGetUtils.Tool.Exec
+    }
+    if(!($toolsList | ?{ $_ -match "dotnet-reportgenerator-globaltool" })) {
+        dotnet tool install -g dotnet-reportgenerator-globaltool
+    }
+}
 
 function Install-NugetCaching() {
     # Enable nuget caching
@@ -179,6 +200,95 @@ Write-Host "password=$($env:GITKEY)"
     }
 }
 
+# TODO: Make this param/variable in project.json somehow?
+function Invoke-NugetSourcesSetup()
+{
+    $nuget = Get-Command nuget -ErrorAction SilentlyContinue
+    $pfRepoUrl = "$($env:LocalNugetServerUrl)"
+    $pfRepoApiKey = "$($env:LocalNugetApiKey)"
+    $pfRepoUser = "$($env:LocalNugetUserName)"
+    $pfRepoPassword = "$($env:LocalNugetPassword)"
+
+    if($IsLinux)
+    {
+        $linkExists = Get-ChildItem ~/.nuget/ | ?{ $_.LinkType -eq "SymbolicLink" -and $_.BaseName -eq "NuGet" }
+        if(!$linkExists)
+        {
+            # Fix issues with mono/dotnet configs in
+            # cat ~/.config/NuGet/NuGet.Config
+            # cat ~/.nuget/NuGet/NuGet.Config
+            # https://github.com/NuGet/Home/issues/4413
+            Remove-Item ~/.nuget/NuGet -Recurse -ErrorAction SilentlyContinue
+            ln -s ~/.config/NuGet/ ~/.nuget/NuGet/
+            Remove-Item ~/.nuget/NuGet/nuget.config -ErrorAction SilentlyContinue
+        }
+    }
+
+    if($nuget)
+    {
+        Write-Host "Checking Nuget sources '$pfRepoUrl'"
+
+        if($pfRepoApiKey) {
+            Write-Host "Setting PowerFarming.Nuget repo"
+            nuget sources add -Name "PowerFarming Nuget" -source "$pfRepoUrl"
+            #nuget setapikey "$pfRepoApiKey" -Source "$pfRepoUrl"
+        } else {
+            Write-Host "Credentials were missing, couldn't set up PowerFarming.Nuget Nuget Source Authentication"
+        }
+        # Needed if running Windows auth
+        if($pfRepoUser) {
+            Write-Host "Setting PowerFarming.Nuget repo user and password override"
+            nuget sources update -Name "PowerFarming Nuget" -UserName "$pfRepoUser" -Password "$pfRepoPassword"
+        }
+    }
+
+    # Try without override
+    if($IsLinux -and $false) {
+        # Set cake config var to main nuget one
+        if(Test-Path "~/.config/NuGet/NuGet.Config") {
+            Write-Host "Setting Cake nuget.config override"
+            $env:CAKE_NUGET_CONFIGFILE = (Resolve-Path "~/.config/NuGet/NuGet.Config").Path
+        }
+    }
+
+    $psRepo = Get-Command Get-PSRepository -ErrorAction SilentlyContinue
+    $enablePSRepo = $false
+    if($psRepo -and $enablePSRepo)
+    {
+        $psGallery = Get-PSRepository -Name "PSGallery"
+        if($psGallery -and $psGallery.InstallationPolicy -ne "Trusted") {
+            Write-Host "Trusting PSGallery PSRepository"
+            Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+        }
+
+        # TODO: Set Authentication?
+        $pfRepo = Get-PSRepository -Name "PowerFarming.Nuget" -ErrorAction SilentlyContinue
+        if(!$pfRepo) {
+            Write-Host "Registering PowerFarming.Nuget PSRepository"
+            Register-PSRepository -Name "PowerFarming.Nuget" -SourceLocation "$pfRepoUrl"
+            $pfRepo = Get-PSRepository -Name "PowerFarming.Nuget" -ErrorAction SilentlyContinue
+        }
+        if($pfRepo -and $pfRepo.InstallationPolicy -ne "Trusted") {
+            Write-Host "Trusting PowerFarming.Nuget PSRepository"
+            Set-PSRepository -Name "PowerFarming.Nuget" -InstallationPolicy Trusted
+        }
+        if($pfRepoApiKey) {
+            $password = "$pfRepoApiKey" | ConvertTo-SecureString -asPlainText -Force
+            $apiCreds = New-Object System.Management.Automation.PSCredential("apikey",$password)
+            Set-PSRepository -Name "PowerFarming.Nuget" -Credential $apiCreds
+        } else {
+            Write-Host "Credentials were missing, couldn't set up PowerFarming.Nuget PSRepository Authentication"
+        }
+    }
+}
+
+function Invoke-CakeBootstrap() {
+    if(Get-Command "dotnet-cake" -ErrorAction SilentlyContinue) {
+        Write-Host "Running cake core bootstrap"
+        dotnet-cake setup.cake --bootstrap
+    }
+}
+
 #===============
 # Main
 #===============
@@ -186,15 +296,17 @@ Write-Host "password=$($env:GITKEY)"
 # Useful missing vars
 $currentBranch = Get-GitCurrentBranch
 $env:BRANCH_NAME=$env:GITBRANCH=$currentBranch
+$isVSTSNode = $env:VSTS_AGENT
+$isJenkinsNode = $env:JENKINS_HOME
 
 Install-PrePrerequisites
 Install-NugetCaching
 Clear-GitversionCache
 Install-DotnetBuildTools
 
-$isVSTSAgent = $env:VSTS_AGENT
-$isJenkinsNode = $env:JENKINS_HOME
-
-if(!($isVSTSAgent) -and !($isJenkinsNode)) {
+if(!($isVSTSNode) -and !($isJenkinsNode)) {
     Invoke-PreauthSetup -FetchBranches:($isJenkinsNode)
 }
+
+Invoke-NugetSourcesSetup
+Invoke-CakeBootstrap
