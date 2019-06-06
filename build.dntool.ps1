2f0b752bd1c347e7e78d3e7b26815b41033342ad
gjonespf <gjones.pf@gmail.com>
2019-06-06 15:36:48 +1200

    Testing linux nuget

--- build.dntool.ps1
+++ build.dntool.ps1
@@ -27,10 +27,40 @@ if(!($env:PATH -match ".dotnet") -and $IsLinux) {
     }
 }
 
-# Until we can get this working
-$env:CAKE_NUGET_USEINPROCESSCLIENT=false
+# TODO: Until we can get this working, manually use Nuget pull
+$brokenInternalNuget = $IsLinux
+if($brokenInternalNuget)
+{
+    $env:CAKE_NUGET_USEINPROCESSCLIENT="false"
+    $TOOLS_DIR = Join-Path $PSScriptRoot "tools"
+    $NUGET_EXE = Join-Path $TOOLS_DIR "nuget.exe"
+    $NUGET_URL = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+    $PACKAGES_CONFIG = Join-Path $TOOLS_DIR "packages.config"
+    $PACKAGES_CONFIG_MD5 = Join-Path $TOOLS_DIR "packages.config.md5sum"
+    
+    if (!(Test-Path $NUGET_EXE)) {
+        Write-Verbose -Message "Downloading missing NuGet.exe..."
+        $NUGET_EXE = Join-Path $TOOLS_DIR "nuget.exe"
+        try {
+            (New-Object System.Net.WebClient).DownloadFile($NUGET_URL, $NUGET_EXE)
+        } catch {
+            Throw "Could not download NuGet.exe."
+        }
+    }
+
+    $NUGET_EXE = (Resolve-Path $NUGET_EXE).Path
+    $ENV:NUGET_EXE = $NUGET_EXE
+    Write-Verbose "Using nuget at: $NUGET_EXE"
+
+    Write-Verbose "Restoring from Nuget"
+    $NuGetOutput = Invoke-Expression "& nuget install -ExcludeVersion -PreRelease -OutputDirectory `"$TOOLS_DIR`" "
+    #$NuGetOutput = Invoke-Expression "& $nugetMono `"$NUGET_EXE`" install -ExcludeVersion -PreRelease -OutputDirectory `"$TOOLS_DIR`" "
+
+
+}
 
 if(Get-Command "dotnet-cake" -ErrorAction SilentlyContinue) {
+    Write-Information "Running dotnet-cake"
     dotnet-cake $Script --target=$Target
 } else {
     Write-Error "Could not find dotnet-cake to run build script"
